e636d62fd12971e490bd31e4031f1ade119419d3
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 23:44:23 +0530

    Update App.js

--- Full stack application/react/src/App.js
+++ Full stack application/react/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';          // Import the logo image (not used in this component)
+import './App.css';                    // Import the CSS file for styling
 
-import { Route, Routes } from 'react-router-dom';
-import VehicleForm from './Components/VehicleForm';
-import VehicleTable from "./Components/VehicleTable"
+import { Route, Routes } from 'react-router-dom';      // Import the Route and Routes components from 'react-router-dom'
+import VehicleForm from './Components/VehicleForm';    // Import the VehicleForm component
+import VehicleTable from "./Components/VehicleTable";  // Import the VehicleTable component
+
 function App() {
   return (
-    <div >
-      <Routes>
-        <Route path="/" element={<VehicleTable></VehicleTable>}></Route>
-        <Route path="/edit/:vid" element={<VehicleForm></VehicleForm>}></Route>
-        <Route path="/form" element={<VehicleForm></VehicleForm>}></Route>
+    <div>
+      <Routes>                                      {/* Use the Routes component to define different routes */}
+        <Route path="/" element={<VehicleTable />} />    {/* Define a route for the root path and render the VehicleTable component */}
+        <Route path="/edit/:vid" element={<VehicleForm />} />  {/* Define a route for the '/edit/:vid' path and render the VehicleForm component */}
+        <Route path="/form" element={<VehicleForm />} />       {/* Define a route for the '/form' path and render the VehicleForm component */}
       </Routes>
-
-
-
-
-     
     </div>
   );
 }
