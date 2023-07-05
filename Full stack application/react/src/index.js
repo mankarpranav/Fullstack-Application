d00365780f9861719c59f87d7b54c5ae99109e29
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 23:43:18 +0530

    Update index.js

--- Full stack application/react/src/index.js
+++ Full stack application/react/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';                        // Import the 'React' module from the 'react' library
+import ReactDOM from 'react-dom';                 // Import the 'ReactDOM' module from the 'react-dom' library
+import './index.css';                             // Import the CSS file for styling
+import App from './App';                          // Import the root component of the application
+import reportWebVitals from './reportWebVitals';   // Import the function for measuring app performance
+import { BrowserRouter } from 'react-router-dom'; // Import the 'BrowserRouter' component from the 'react-router-dom' library
+
+// Create a root element for rendering the React app
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the app wrapped in BrowserRouter and StrictMode
 root.render(
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>                         {/* Use BrowserRouter component to enable routing in the app */}
+    <React.StrictMode>                     {/* Use StrictMode to enable additional development checks */}
+      <App />                              {/* Render the root component of the application */}
+    </React.StrictMode>
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance in the app and log results
 reportWebVitals();
