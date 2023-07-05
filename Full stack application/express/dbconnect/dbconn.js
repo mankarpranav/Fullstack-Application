9f4771190b1781400636e4f4f0cef8b85a945212
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 23:35:44 +0530

    Update dbconn.js

--- Full stack application/express/dbconnect/dbconn.js
+++ Full stack application/express/dbconnect/dbconn.js
@@ -1,30 +1,23 @@
+// Import the 'mysql' module
+const mysql = require("mysql");
 
+// Create a connection to the MySQL database
+const mysqlconn = mysql.createConnection({
+  host: "127.0.0.1",    // MySQL server IP address
+  user: "root",         // MySQL username
+  password: "root",    // MySQL password
+  port: 3306,           // MySQL server port
+  database: "test"      // MySQL database name
+});
 
+// Connect to the MySQL database
+mysqlconn.connect((err) => {
+  if (err) {
+    console.log(err);   // Print any error that occurred during connection
+  } else {
+    console.log("Connection successful");   // Print a success message on successful connection
+  }
+});
 
-
-
-const mysql=require("mysql")
-
-const mysqlconn=mysql.createConnection({
-  "host":"127.0.0.1",
-  "user":"root",
-  "password":"12345",
-  "port":3306,
-  "database":"test"
-
-
-})
-
-mysqlconn.connect((err)=>{
-    if(err){
-        console.log(err)
-    }
-    else{
-        console.log("connection done")
-    }
-})
-
-
-
-
-module.exports=mysqlconn
+// Export the MySQL connection for use in other modules
+module.exports = mysqlconn;
