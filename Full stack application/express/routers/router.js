fc0410ab4bb2173ed4bd213019b17ea915e5d475
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 23:37:25 +0530

    Update router.js

--- Full stack application/express/routers/router.js
+++ Full stack application/express/routers/router.js
@@ -1,74 +1,78 @@
+const express = require("express");
+const router = express.Router();
 
+const conn = require("../dbconnect/dbconn");
 
-
-const express=require("express")
-const router= express.Router();
-
-const conn=require("../dbconnect/dbconn");
-
-
-
+// Get vehicle by ID
 router.get("/vehicles/:vid", (req, resp) => {
-    console.log("in get by id")
-    conn.query(
-      "select * from veh where vid=?",
-      [req.params.vid],
-      (err, data) => {
-        if (err) {
-          resp.status(500).send(JSON.stringify("data not found" + err));
-          console.log(JSON.stringify(err));
-        } else {
-          resp.send(data[0]);
-        }
+  console.log("In get by ID");
+  conn.query(
+    "SELECT * FROM veh WHERE vid=?",
+    [req.params.vid],
+    (err, data) => {
+      if (err) {
+        resp.status(500).send(JSON.stringify("Data not found: " + err));
+        console.log(JSON.stringify(err));
+      } else {
+        resp.send(data[0]);
       }
-    );
+    }
+  );
+});
+
+// Get all vehicles
+router.get("/vehicles", (req, resp) => {
+  conn.query("SELECT * FROM veh", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resp.send(data);
+    }
   });
-router.get("/vehicles",(req,resp)=>{
-    conn.query("select * from veh",(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            resp.send(data)
-        }
-    })
-})
+});
 
-router.delete("/vehicles/delete/:vid",(req,resp)=>{
-    console.log("in delete")
-    conn.query("delete from veh where vid=?",[req.params.vid],(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            resp.send("delted successfully")
-        }
-    })
-})
+// Delete vehicle by ID
+router.delete("/vehicles/delete/:vid", (req, resp) => {
+  console.log("In delete");
+  conn.query("DELETE FROM veh WHERE vid=?", [req.params.vid], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resp.send("Deleted successfully");
+    }
+  });
+});
 
-router.post("/vehicles/vehicle/",(req,resp)=>{
-    console.log("in post");
-    conn.query("insert into veh values(?,?,?)",[req.body.vid,req.body.price,req.body.vname],(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            resp.send(data)
-        }
-    })
-})
+// Create a new vehicle
+router.post("/vehicles/vehicle", (req, resp) => {
+  console.log("In post");
+  conn.query(
+    "INSERT INTO veh VALUES (?, ?, ?)",
+    [req.body.vid, req.body.price, req.body.vname],
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resp.send(data);
+      }
+    }
+  );
+});
 
-router.put("/vehicles/vehicle/",(req,resp)=>{
-    console.log("in update ");
-    conn.query("update veh set price=?, vname=? where vid=?",[req.body.price,req.body.vname,req.body.vid],(err,data)=>{
-        if(err){
-            console.log(err)
-        }
-        else{
-            resp.send(data)
-        }
-    })
-})
+// Update vehicle
+router.put("/vehicles/vehicle", (req, resp) => {
+  console.log("In update");
+  conn.query(
+    "UPDATE veh SET price=?, vname=? WHERE vid=?",
+    [req.body.price, req.body.vname, req.body.vid],
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        resp.send(data);
+      }
+    }
+  );
+});
 
-
-module.exports=router
+module.exports = router;
