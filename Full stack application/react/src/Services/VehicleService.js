9ea87a6009305cf2efde9a1ef63c9b5fbead4a80
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-05 23:45:20 +0530

    Update VehicleService.js

--- Full stack application/react/src/Services/VehicleService.js
+++ Full stack application/react/src/Services/VehicleService.js
@@ -1,41 +1,34 @@
-import axios from 'axios'
+import axios from 'axios';          // Import the 'axios' library for making HTTP requests
 
+const baseurl = "http://localhost:3005";   // Define the base URL for API requests
 
+class VehicleService {
+  // Get vehicle by ID
+  getVehiclebyId = (id) => {
+    return axios.get(baseurl + "/vehicles/" + id);   // Make a GET request to retrieve vehicle data by ID
+  };
 
-const baseurl="http://localhost:3005"
-class VehicleService{
+  // Get all vehicles
+  getVehicles = () => {
+    return axios.get(baseurl + "/vehicles/");   // Make a GET request to retrieve all vehicle data
+  };
 
+  // Delete vehicle by ID
+  deleteById(id) {
+    return axios.delete(baseurl + "/vehicles/delete/" + id);   // Make a DELETE request to delete a vehicle by ID
+  }
 
+  // Insert a new vehicle
+  insertVeh(veh) {
+    console.log("In insertVeh: " + veh.vid);
+    return axios.post(baseurl + "/vehicles/vehicle/", veh);   // Make a POST request to insert a new vehicle
+  }
 
-    getVehiclebyId=(id)=>{
-
-        return axios.get(baseurl+"/vehicles/"+id)
-
-    }
-    getVehicles=()=>{
-
-        return axios.get(baseurl+"/vehicles/")
-
-    }
-
-    deleteById(id){
-        return axios.delete(baseurl+"/vehicles/delete/"+id)
-    }
-
-
-    insertVeh(veh){
-        console.log("in insertveh "+veh.vid)
-        return axios.post(baseurl+"/vehicles/vehicle/",veh)
-    }
-
-    updateVeh(veh){
-        console.log("in insertveh "+veh.vid)
-        return axios.put(baseurl+"/vehicles/vehicle/",veh)
-    }
-    
-
-
+  // Update an existing vehicle
+  updateVeh(veh) {
+    console.log("In updateVeh: " + veh.vid);
+    return axios.put(baseurl + "/vehicles/vehicle/", veh);   // Make a PUT request to update an existing vehicle
+  }
 }
 
-
-export  default new VehicleService;
+export default new VehicleService;
